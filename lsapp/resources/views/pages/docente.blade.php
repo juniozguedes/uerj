9ee0830b46d7d06bf6f521b3febe5096ff61ff7d
Wellington <wellington.guedes@afil.io>
2019-11-29 19:55:03 -0300

    Ajuste extra docente

--- lsapp/resources/views/pages/docente.blade.php
+++ lsapp/resources/views/pages/docente.blade.php
@@ -5,7 +5,7 @@
       <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center">
           <div class="col-md-9 ftco-animate text-center">
-            <h1 class="mb-2 bread">Corpo Docente</h1>
+            <h1 class="mb-2 bread">{{__('Corpo Docente')}}</h1>
             <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Corpo Docente <i class="ion-ios-arrow-forward"></i></span></p>
           </div>
         </div>
