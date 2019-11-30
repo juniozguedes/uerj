8f80e02d9225e70ddfff217c2d9c4da17fbb83da
Wellington <wellington.guedes@afil.io>
2019-11-30 00:03:28 -0300

    Normalized

--- lsapp/database/migrations/2019_07_26_151257_create_posts_table.php
+++ lsapp/database/migrations/2019_07_26_151257_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->mediumText('body');
+            $table->mediumText('body')->nullable(True);
             $table->timestamps();
         });
     }
