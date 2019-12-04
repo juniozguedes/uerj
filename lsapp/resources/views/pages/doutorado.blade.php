6d07f2d29be267509bd9b3315021b6fae78575a4
Wellington <wellington.guedes@afil.io>
2019-12-04 12:12:03 -0300

    Ajustes de doutorado, logos inclusas

--- lsapp/resources/views/pages/doutorado.blade.php
+++ lsapp/resources/views/pages/doutorado.blade.php
@@ -1,8 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-	<h1>Doutorado</h1>
+
+<div class="container" style="padding: 30px;">
+  <div class="row">
+    <div class="col-md-4" style="text-align: center;">
+      <img src="{{ url('images/doutorado_logo_1.png') }}">
+    </div>
+    <div class="col-md-4" style="text-align: center;">
+                          <p>UNIVERSIDADE DO ESTADO DO RIO DE JANEIRO</p>
+                          <p>CENTRO DE CIÊNCIAS SOCIAIS</p>
+                          <p>FACULDADE DE SERVIÇO SOCIAL</p>
+                          <p>PROGRAMA DE PÓS-GRADUAÇÃO EM SERVIÇO SOCIAL</p>
+    </div>
+    <div class="col-md-4 "style="text-align: center;">
+      <img src="{{ url('images/doutorado_logo_2.png') }}">
+    </div>
+  </div>
 </div>
+<div class="container" style="text-align: center;">
+  <div class="row">
+    <div class="col-md-12">
 @if(count($posts) > 0)
       @foreach($posts as $post)
       <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
@@ -19,5 +36,7 @@
   @else
     <p>Não há postagens</p>
   @endif
-
+  </div>
+  </div>
+</div>
 @endsection
